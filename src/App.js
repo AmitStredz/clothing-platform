2662ac2d1dfea92582af6cbbcbe09ce3595106f6
Muhammad Althaf P A <althafmuhammad2115@gmail.com>
2025-03-07 10:55:13 +0530

    updated product listing compoent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import ProductCard from './Shared/product-card';
 import ProductList from './Shared/product-list';
 
 function App() {
   return (
     <div className="App">
       {/* <span className='text-5xl'>Clothing Platform</span> */}
-      <ProductCard />
+      <ProductList/>
     </div>
   );
 }
--- src/Shared/product-list.js
+++ src/Shared/product-list.js
@@ -1,6 +1,14 @@
-function ProductList(){
-    return(
-        <h1>jhbdsj</h1>
-    )
+import ProductCard from "./product-card";
+
+function ProductList() {
+  return (
+    <div className="flex gap-10 flex-wrap">
+      {[...Array(10)].map((_, index) => (
+          <div key={index}>
+            <ProductCard />
+          </div>
+      ))}
+    </div>
+  );
 }
 export default ProductList;
